5793d569f76ffb388c3d83bf11fa43d6f24719ea
praveenkumar09 <tpkprav@gmail.com>
2024-11-30 18:08:51 +0800

    Initial commit

---
+++ src/main/java/com/udemy/utils/CommonUtils.java
@@ -0,0 +1,21 @@
+package com.udemy.utils;
+
+import java.time.Duration;
+
+public class CommonUtils {
+
+    public static void sleep(Duration duration) {
+        try {
+            Thread.sleep(duration);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static long timer(Runnable runnable) {
+        var start = System.currentTimeMillis();
+        runnable.run();
+        var end = System.currentTimeMillis();
+        return (end - start);
+    }
+}
