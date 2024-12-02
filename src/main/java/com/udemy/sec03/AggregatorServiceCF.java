04003605022bd4cadd393fe06f9ec0bd0ba0d41e
praveenkumar09 <tpkprav@gmail.com>
2024-12-02 21:04:23 +0800

    Aggregator demo added for the Completable Future

--- src/main/java/com/udemy/sec03/AggregatorServiceCF.java
+++ src/main/java/com/udemy/sec03/AggregatorServiceCF.java
@@ -6,6 +6,7 @@ import com.udemy.sec02.externalservice.Client;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class AggregatorServiceCF {
 
@@ -17,9 +18,13 @@ public class AggregatorServiceCF {
 
     public Product getProduct(int id) throws ExecutionException, InterruptedException {
         var product = CompletableFuture.supplyAsync(() -> Client.getProduct(id), executorService)
-                .exceptionally(ex -> "Product not found");
+                .exceptionally(ex -> "Product not found")
+                .orTimeout(750, TimeUnit.MILLISECONDS)
+                .exceptionally(ex -> "Product timeout");
         var rating = CompletableFuture.supplyAsync(() -> Client.getRating(id), executorService)
-                .exceptionally(ex -> -1);
+                .exceptionally(ex -> -1)
+                .orTimeout(750, TimeUnit.MILLISECONDS)
+                .exceptionally(ex -> -2);
         return new Product(id, product.get(), rating.get());
     }
 }
